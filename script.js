51fddfd2f4c73edd80365f88134c940a2ef6c4c7
Yadu Krishnan <yaduk.tv@gmail.com>
2023-03-10 13:36:43 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,40 @@
+//to display no location warning sign
+function revealNoLocationWarning () {
+        document.getElementById("weatherNotFound").style.display = 'block';
+        document.getElementById("weatherFound").style.display = 'none';
+}
+
+//to display the weather data
+function revealLocation () {
+    document.getElementById("weatherNotFound").style.display = 'none';
+    document.getElementById("weatherFound").style.display = 'block';
+}
+
+//to click the button on pressing enter key
+window.onload = function() {
+    var input = document.getElementById("location");
+
+    input.addEventListener("keypress", function(event) {
+        if( event.key === "Enter" ) {
+            event.preventDefault();
+            document.getElementById("location-button").click();
+        }
+    });
+}
+
+//checks if location exists and calls revealLocation or revealNoLocationWarning
+function checkLocation () {
+    var x = document.getElementById("location").value;
+    x.toString();
+    var yes = 'yes';
+    yes.toString();
+
+    if( x === yes ) {
+        revealLocation();
+    } else {
+        revealNoLocationWarning();
+    }
+}
+
+
+const api_key = "e86b10876f017da658a0a925e5312540";
